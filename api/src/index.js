e4b18b218855b3b9d8a099592b9e39dcd06f5ea4
John Leonardo <j@jdleo.me>
2021-09-04 22:47:16 -0700

    entry

---
+++ api/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+import baseRoute from './routes/base';
+import pollRoute from './routes/poll';
+
+// express app
+const app = express();
+
+// configure port for api
+app.set('port', process.env.PORT || 3000);
+
+// middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// map routes to routers
+app.use('/', baseRoute);
+app.use('/poll', pollRoute);
+
+// listen for requests
+app.listen(app.get('port'), () => {
+  console.info(`api server is listening on port ${app.get('port')}`);
+});
