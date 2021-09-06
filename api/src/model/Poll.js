92059c5ad36b5e013883802dcd0a9add3e4e83d2
John Leonardo <j@jdleo.me>
2021-09-05 20:14:08 -0700

    update poll model

--- api/src/model/Poll.js
+++ api/src/model/Poll.js
@@ -3,12 +3,12 @@ const Sequelize = require('sequelize');
 
 // create poll model
 const Poll = db.define('poll', {
+  id: { primaryKey: true, type: Sequelize.STRING },
   title: Sequelize.STRING,
   options: Sequelize.ARRAY(Sequelize.STRING),
   expiration: Sequelize.BIGINT,
   limit_ip: Sequelize.BOOLEAN,
   api_key: Sequelize.STRING,
-  created_at: Sequelize.DATE,
 });
 
 module.exports = Poll;
