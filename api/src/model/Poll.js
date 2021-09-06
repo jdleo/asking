4d613b96f54915fdf725fbe60cd740d0789c46ef
John Leonardo <j@jdleo.me>
2021-09-05 18:30:02 -0700

    poll model

---
+++ api/src/model/Poll.js
@@ -0,0 +1,14 @@
+const db = require('../connections');
+const Sequelize = require('sequelize');
+
+// create poll model
+const Poll = db.define('poll', {
+  title: Sequelize.STRING,
+  options: Sequelize.ARRAY(Sequelize.STRING),
+  expiration: Sequelize.BIGINT,
+  limit_ip: Sequelize.BOOLEAN,
+  delete_key: Sequelize.STRING,
+  created_at: Sequelize.DATE,
+});
+
+module.exports = Poll;
