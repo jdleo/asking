b3514bc3747bed426af43d91d16ecafd3cbeae06
John Leonardo <j@jdleo.me>
2021-09-05 20:17:35 -0700

    add vote model

---
+++ api/src/model/Vote.js
@@ -0,0 +1,14 @@
+const db = require('../connections');
+const Sequelize = require('sequelize');
+const Poll = require('./Poll');
+
+// create vote model
+const Vote = db.define('vote', {
+  id: { primaryKey: true, type: Sequelize.STRING },
+  ip_address: Sequelize.STRING,
+});
+
+// add foreign key relationship to poll
+Vote.belongsTo(Poll);
+
+module.exports = Vote;
