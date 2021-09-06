39dbbd1b97948f149dfe66eb97de7b6ed27dc8c6
John Leonardo <j@jdleo.me>
2021-09-05 20:42:34 -0700

    get poll : in progress

--- api/src/controller/poll/getPoll.js
+++ api/src/controller/poll/getPoll.js
@@ -1,7 +1,33 @@
+const Poll = require('../../model/Poll');
+
 /**
  * @param {object} req - request object
  * @param {object} res - response object
  */
-const getPoll = (req, res) => {};
+const getPoll = (req, res) => {
+  // get poll id from request
+  const pollId = req.params.id;
+
+  // verify param is valid
+  if (pollId === undefined) {
+    return res.status(400).json({
+      status: 'error',
+      error: 'Invalid request.',
+    });
+  }
+
+  // look up associated poll
+  Poll.findByPk(pollId)
+    .then(poll => {
+      // send poll back to client
+      return res.status(200).json(poll.get({ plain: true }));
+    })
+    .catch(() => {
+      res.status(404).json({
+        status: 'error',
+        error: 'Poll not found with supplied ID.',
+      });
+    });
+};
 
 module.exports = getPoll;
