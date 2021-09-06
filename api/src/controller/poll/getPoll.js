27be0e20a410cf8e07273dccb81df3772d9bf851
John Leonardo <j@jdleo.me>
2021-09-05 23:36:40 -0700

    get poll now has vote counts

--- api/src/controller/poll/getPoll.js
+++ api/src/controller/poll/getPoll.js
@@ -1,4 +1,6 @@
 const Poll = require('../../model/Poll');
+const db = require('../../connections');
+const { QueryTypes } = require('sequelize');
 
 /**
  * @param {object} req - request object
@@ -19,8 +21,42 @@ const getPoll = (req, res) => {
   // look up associated poll
   Poll.findByPk(pollId)
     .then(poll => {
-      // send poll back to client
-      return res.status(200).json(poll.get({ plain: true }));
+      // get fields that we should expose
+      const title = poll.get('title');
+      const options = poll.get('options');
+      const createdAt = poll.get('createdAt');
+      const expiration = poll.get('expiration');
+
+      // raw query string
+      let query = `
+        SELECT option, count(option) as vote_count
+        FROM votes
+        WHERE "pollId" = '${pollId}'
+        GROUP BY option;
+      `;
+
+      // execute query
+      db.query(query, { type: QueryTypes.SELECT })
+        .then(res => {
+          // return poll data
+          return res.status(200).json({
+            status: 'success',
+            data: {
+              title,
+              options,
+              createdAt,
+              expiration,
+              votes: res,
+            },
+          });
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).json({
+            status: 'error',
+            error: 'Unable to retrieve poll results.',
+          });
+        });
     })
     .catch(() => {
       res.status(404).json({
