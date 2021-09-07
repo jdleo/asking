19947b846aa9254a8f2e5158288ff007aa6c70c6
John Leonardo <j@jdleo.me>
2021-09-06 17:15:56 -0700

    add deployment

--- poll-for-deployment.sh
+++ poll-for-deployment.sh
@@ -8,7 +8,10 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    # pull fresh code
+    git pull;
+    # deploy using docker-compose
+    docker-compose -f docker-compose.prod.yml up -d --build;
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
 else
